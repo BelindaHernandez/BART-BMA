7f441847b42a2ee26b3075cddaa6a7808a0893a2
BelindaHernandez <belindahernandez25@gmail.com>
2015-04-02 15:55:20 +0100

    Update CALLBARTBMA4.R

--- R/CALLBARTBMA4.R
+++ R/CALLBARTBMA4.R
@@ -32,7 +32,11 @@ BART_BMA4=function(x.train,y.train,a=3,nu=3,sigquant=0.9,c=1000,
       if(!is.matrix(x.test)) stop('x.test must be a matrix')
     } 
   }
+  
+  
   #check input arguments:
+  # if((!is.matrix(x.train)) || (typeof(x.train)!="double")) stop("argument x.train must be a double matrix")
+  #if((!is.matrix(x.test)) || (typeof(x.test)!="double")) stop("argument x.test must be a double matrix")
   if((!is.matrix(x.train))) stop("argument x.train must be a double matrix")
   if((!is.matrix(x.test)) ) stop("argument x.test must be a double matrix")
   if(!binary) {
@@ -40,10 +44,13 @@ BART_BMA4=function(x.train,y.train,a=3,nu=3,sigquant=0.9,c=1000,
   }
   if(nrow(x.train) != length(y.train)) stop("number of rows in x.train must equal length of y.train")
   if((nrow(x.test) >0) && (ncol(x.test)!=ncol(x.train))) stop("input x.test must have the same number of columns as x.train")
-  
+  #if((!is.na(sigest)) && (typeof(sigest)!="double")) stop("input sigest must be double")
+  #if((!is.na(sigest)) && (sigest<0.0)) stop("input sigest must be positive")
+  #if((mode(sigdf)!="numeric") || (sigdf<0)) stop("input sigdf must be a positive number")
   if(c<1)stop("Value of Occam's Window has to be greater than 0."); 
   if(num_cp<0 || num_cp>100)stop("Value of num_cp should be a value between 1 and 100."); 
   
+  # sourceCpp("010914_BART_BMA_test_data_PELTmeanvar_scaledresponse6.cpp")  
   start=proc.time()
   test_BMA34=BART_BMA(x.train,y.train,start_mean,start_sd,a,mu,nu,lambda,c,sigma_mu,
                       pen,num_cp,x.test,num_rounds,alpha,beta,split_rule_node,gridpoint,maxOWsize)
