61a6be4d0607ba3f5847a10487cbbede0b195b36
BelindaHernandez <belindahernandez25@gmail.com>
2015-04-02 16:41:21 +0100

    Create find_term_nodes.h

---
+++ src/find_term_nodes.h
@@ -0,0 +1,14 @@
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+using namespace Rcpp;
+// [[Rcpp::export]]
+
+NumericVector find_term_nodes(NumericMatrix tree_table){
+  //NumericVector terminal_nodes;
+  arma::mat arma_tree(tree_table.begin(),tree_table.nrow(), tree_table.ncol(), false); 
+  
+  arma::vec colmat=arma_tree.col(4);
+  arma::uvec term_nodes=arma::find(colmat==-1);
+  term_nodes=term_nodes+1;
+  return(wrap(term_nodes));
+}
